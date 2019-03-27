4901866f0af1b9d49b561bfa1f1dea18d44c3b32
Pedro Henrique Santos <pehensil@yahoo.com.br>
2019-03-27 15:58:09 -0300

    modified euler implemented

--- methods.py
+++ methods.py
@@ -26,6 +26,18 @@ def mult_step(x, y, n, h):
     for i in range(n-2):
         y.append(-40 * h * y[i+1] + y[i])
 
+def modifiedEuler(x, y, n, h):
+    for i in range(n-1):
+        yAux = (y[i] + (-y[i] + x[i] + 1/2) * h)  # ũ Explicito
+        # y.append(y[i] / (-yAux + x[i+1] + 1/2) * h) # Implicito
+        y.append(y[i] + (h/2.0) * ((-y[i] + x[i] + 1/2) - yAux + x[i] + 1/2)) #
+
+def analitico(x, y):  # u' = -u + t + 1/2
+    for t in np.arange(0, 1.001, 0.01):
+        x.append(t)
+        y.append(t + 3 / 2 * np.exp(-t) - 1 / 2)
+    return x, y
+
 if __name__ == '__main__':
     deltas = [1/9, 1/10, 1/11, 1/20]
     y_names = ["y_ee", "y_ie", "y_trap", "y_mult"]
@@ -34,11 +46,12 @@ if __name__ == '__main__':
     y_colors = ["blue", "red", "darkorange", "green"]
     # print(deltas)
 
+    # First exercise
     x_analitic = np.linspace(0, 2, 100)
     y_analitic = []
     analitic(x_analitic, y_analitic)
 	
-	
+    """
     for i in range(len(deltas)):
         y = [[2] for j in range(4)]
         n = int((2.0 - 0.0)/deltas[i]) + 1
@@ -55,3 +68,19 @@ if __name__ == '__main__':
             plt.title(y_names[j]+ " delta="+str(deltas[i]))
             plt.autoscale(enable=True, axis='both', tight=None)
             plt.show()
+    """
+
+    # Code for modifiedEuler
+    x_analitic = []
+    y_analitic = []
+    x_analitic, y_analitic = analitico(x_analitic, y_analitic)
+    h = 0.1
+    y = [1]
+    n = int((1.0 - 0.0) / h) + 1
+    x = np.linspace(0., 1., n)
+    modifiedEuler(x, y, n, h)
+    plt.plot(x_analitic, y_analitic, color="black")
+    plt.plot(x, y, label="Modified Euler", color="red")
+    plt.legend(["Solução Analítica", "Euler Modificado"])
+    plt.title("")
+    plt.show()
